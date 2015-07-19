d4eda6f2084451cb2a1fd5829024f9e48b23c653
Bananaft <Bananaft@gmail.com>
2015-07-19 17:37:20 +0500

    exponentas

--- bin/Data/Shaders/GLSL/Sky.glsl
+++ bin/Data/Shaders/GLSL/Sky.glsl
@@ -42,19 +42,23 @@ void PS()
 
     vec4 projWorldPos = vec4(worldPos, 1.0);
 
-    float depth2 = length(worldPos.xz);
+    float depth2 = 1-clamp(length(worldPos)/7000, 0.0, 1.0);
 
-    float fogFactor = clamp(depth2 / 7000, 0.0, 1.0);
-    float diffFactor = clamp(depth2 / 12000, 0.0, 1.0);
+   float fogFactor = (1-exp(depth2*4-4));
+    float diffFactor = (exp(depth2*3-3));
+    //float skyfactor = (exp((1-depth2)*10-10));
 
     //float skydiff = 0.5 * (normal.y + 1.0);
     vec3 DirRay = normalize(vFarRay);
+    float layer = min(exp((1-abs(DirRay.y))*10*(1-diffFactor)-10*(1-diffFactor)),1);
 
-    float sunAmount = max (dot(DirRay ,-1 * cSunDir ),0);
+    float sunAmount = max(dot(DirRay ,-1 * cSunDir ),0);// * (1-diffFactor);//max (dot(DirRay ,-1 * cSunDir ),0);
+    //sunAmount *= 1+layer*2;
+    //
 
-    vec3 fogcolor =  cSkyColor + cSunColor *  pow(sunAmount,8.0);//mix( cSkyColor,cSunColor, sunAmount );// pow(sunAmount,8.0)
+    vec3 fogcolor = cSkyColor + cSunColor *  pow(sunAmount,(1-layer)*8.0);//mix( cSkyColor,cSunColor, sunAmount );// pow(sunAmount,8.0)
 
-    vec3 result = diffuseInput.rgb * (1-0.95*diffFactor) + fogcolor * fogFactor;
+    vec3 result = diffuseInput.rgb*diffFactor + fogcolor*fogFactor; //diffuseInput.rgb * (1-0.95*diffFactor) + fogcolor * fogFactor;
 
     gl_FragColor = vec4(result, 0.0);
 
