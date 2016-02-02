4608696e2bf7d10767090fc9f9598d31b73665b1
Greg <Bananaft@gmail.com>
2016-02-02 19:01:14 +0500

    imported models

--- bin/importModels.bat
+++ bin/importModels.bat
@@ -1,1 +1,3 @@
 AssetImporter.exe model ..\assets\models\vehicles\cars\hilux.dae Data\Models\Vehicles\Cars\hilux -nm -nt
+AssetImporter.exe model ..\assets\models\vehicles\aircrafts\f16.dae Data\Models\vehicles\aircrafts\f16 -nm -nt
+pause
